a3a085c08aca5cc54b8cad7a3b1574da709b1f5a
Sardorbek <sardorbekmatniyazov03@gmail.com>
2022-09-01 01:04:02 +0500

    outcome history

--- src/main/java/demobreadshop/repository/InputRepository.java
+++ src/main/java/demobreadshop/repository/InputRepository.java
@@ -15,18 +15,18 @@ public interface InputRepository extends JpaRepository<Input, Long> {
     List<Input> findAllByType(ProductType type);
 
     @Query(
-            value = "select sum(i.material_amount) as amount, u.full_name as name, u.user_kpi as kpi, sum(i.material_amount * u.user_kpi) as sum, u.users_id as userId\n" +
-                    "from input i join (users u join users_roles ur on u.id = ur.users_id) u on i.created_by = u.full_name\n" +
-                    "where u.roles_id = ?1 and i.input_type = 'PRODUCT'\n" +
+            value = "select sum(i.material_amount) as amount, u.full_name as name, u.user_kpi as kpi, sum(i.material_amount * u.user_kpi) as sum, u.users_id as userId " +
+                    "from input i join (users u join users_roles ur on u.id = ur.users_id) u on i.created_by = u.full_name " +
+                    "where u.roles_id = ?1 and i.input_type = 'PRODUCT' " +
                     "group by u.full_name, u.user_kpi, u.users_id",
             nativeQuery = true
     )
     List<GroupStatistics> getAllGroupStatistics(Long roleId, Timestamp time, Timestamp timestamp);
 
     @Query(
-            value = "select wh.id as productId, sum(i.material_amount) as amount, wh.wh_name as name, wh.updated_at as updatedAt, sum(i.material_amount * i.product_price) as sum\n" +
-                    "from input i join ware_house wh on wh.id = i.material_id\n" +
-                    "where wh.product_type = ?1 and i.created_at >= ?2 and i.created_at <= ?3\n" +
+            value = "select wh.id as productId, sum(i.material_amount) as amount, wh.wh_name as name, wh.updated_at as updatedAt, sum(i.material_amount * i.product_price) as sum " +
+                    "from input i join ware_house wh on wh.id = i.material_id " +
+                    "where wh.product_type = ?1 and i.created_at >= ?2 and i.created_at <= ?3 " +
                     "group by wh.id",
             nativeQuery = true
     )
--- src/main/java/demobreadshop/repository/SaleRepository.java
+++ src/main/java/demobreadshop/repository/SaleRepository.java
@@ -42,11 +42,11 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     // isletilip atrg'an ingredientler statisticasi l
     @Query(
-            value = "with bum as (select sum(input.material_amount * pl.material_amount) as sum, pl.material_id as material_id\n" +
-                    "from input join product_list pl on input.material_id = pl.warehouse_fk\n" +
-                    "where input.created_at >= ?1 and input.created_at <= ?2\n" +
-                    "group by pl.material_id)\n" +
-                    "select bum.sum as sum, w.wh_name as name, w.wh_price as price, bum.sum * w.wh_price as wholePrice, w.id\n" +
+            value = "with bum as (select sum(input.material_amount * pl.material_amount) as sum, pl.material_id as material_id " +
+                    "from input join product_list pl on input.material_id = pl.warehouse_fk " +
+                    "where input.created_at >= ?1 and input.created_at <= ?2 " +
+                    "group by pl.material_id) " +
+                    "select bum.sum as sum, w.wh_name as name, w.wh_price as price, bum.sum * w.wh_price as wholePrice, w.id " +
                     "from bum inner join ware_house w on bum.material_id = w.id",
             nativeQuery = true
     )
@@ -54,23 +54,23 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     // seller lar jiynap kelgen aqcha
     @Query(
-            value = "with bum as\n" +
-                    "(select sum(pa.archive_amount) as sum, s.id, s.created_by, s.created_at\n" +
-                    " from sale s join pay_archive pa on s.id = pa.sale_id\n" +
-                    " where s.created_at >= ?1 and s.created_at <= ?2\n" +
-                    " group by s.id, s.created_by)\n" +
-                    "select sum(bum.sum) as sumAmount, bum.created_by as fullName, u.phone_number, u.users_id as userId\n" +
-                    "from bum join (users u join users_roles ur on u.id = ur.users_id) u on bum.created_by = u.full_name\n" +
-                    "group by u.phone_number, bum.created_by, u.users_id;",
+            value = "with bum as " +
+                    "(select sum(pa.archive_amount) as sum, s.id, s.created_by, s.created_at " +
+                    " from sale s join pay_archive pa on s.id = pa.sale_id " +
+                    " where s.created_at >= ?1 and s.created_at <= ?2 " +
+                    " group by s.id, s.created_by) " +
+                    "select sum(bum.sum) as sumAmount, bum.created_by as fullName, u.phone_number, u.users_id as userId " +
+                    "from bum join (users u join users_roles ur on u.id = ur.users_id) u on bum.created_by = u.full_name " +
+                    "group by u.phone_number, bum.created_by, u.users_id",
             nativeQuery = true
     )
     List<SellerStatistics> getAllUserStatistics(Timestamp time, Timestamp timestamp);
 
     // clientler statisticasi grafic kim ko'p sawda ishladi
     @Query(
-            value = "select c.full_name as fullName, c.phone_number as phoneNumber, c.id as clientId, sum(s.whole_price) as wholePrice\n" +
-                    "from sale s join client c on c.id = s.client_id\n" +
-                    "where s.created_at >= ?1 and s.created_at <= ?2\n" +
+            value = "select c.full_name as fullName, c.phone_number as phoneNumber, c.id as clientId, sum(s.whole_price) as wholePrice " +
+                    "from sale s join client c on c.id = s.client_id " +
+                    "where s.created_at >= ?1 and s.created_at <= ?2 " +
                     "group by c.created_by, c.id;",
             nativeQuery = true
     )
@@ -78,9 +78,9 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     // kpi bo'yincha qo'shilg'an summa istoriyasi
     @Query(
-            value = "select sum(o.material_amount * u.user_kpi) as amount, o.created_at as createdAt\n" +
-                    "from output o join users u on o.created_by = u.full_name\n" +
-                    "where output_type = 'O_SALE' and u.id = ?1 and o.created_at >= ?2 and o.created_at <= ?3\n" +
+            value = "select sum(o.material_amount * u.user_kpi) as amount, o.created_at as createdAt " +
+                    "from output o join users u on o.created_by = u.full_name " +
+                    "where output_type = 'O_SALE' and u.id = ?1 and o.created_at >= ?2 and o.created_at <= ?3 " +
                     "group by o.created_at;",
             nativeQuery = true
     )
@@ -88,12 +88,12 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     @Query(
             value = "with bum as (\n" +
-                    "select sum(pa.archive_amount) as amount, s.id, pa.pay_type\n" +
-                    "from sale s join pay_archive pa on s.id = pa.sale_id\n" +
-                    "group by pa.pay_type, s.id\n" +
-                    ") select sum(bum.amount) as amount, bum.pay_type as payType\n" +
-                    "from sale s join bum on bum.id = s.id\n" +
-                    "where s.created_at >= ?1 and s.created_at <= ?2\n" +
+                    "select sum(pa.archive_amount) as amount, s.id, pa.pay_type " +
+                    "from sale s join pay_archive pa on s.id = pa.sale_id " +
+                    "group by pa.pay_type, s.id " +
+                    ") select sum(bum.amount) as amount, bum.pay_type as payType " +
+                    "from sale s join bum on bum.id = s.id " +
+                    "where s.created_at >= ?1 and s.created_at <= ?2 " +
                     "group by bum.pay_type;",
             nativeQuery = true
     )
@@ -102,11 +102,11 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
     // clientlarding uliwma bergan summalari
     @Query(
             value = "with bum as (\n" +
-                    "select sum(pa.archive_amount) as amount, s.id, pa.pay_type, client_id\n" +
-                    "from sale s join pay_archive pa on s.id = pa.sale_id\n" +
-                    "group by pa.pay_type, s.id\n" +
-                    ") select sum(bum.amount) as amount, c.full_name\n" +
-                    "from client c join bum on bum.client_id = c.id\n" +
+                    "select sum(pa.archive_amount) as amount, s.id, pa.pay_type, client_id " +
+                    "from sale s join pay_archive pa on s.id = pa.sale_id " +
+                    "group by pa.pay_type, s.id " +
+                    ") select sum(bum.amount) as amount, c.full_name " +
+                    "from client c join bum on bum.client_id = c.id " +
                     "group by c.full_name;",
             nativeQuery = true
     )
@@ -114,18 +114,18 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     // qarizdarlar sani
     @Query(
-            value = "select count(c.id), c.full_name\n" +
-                    "from sale s join client c on s.client_id = c.id\n" +
-                    "where s.status = 'DEBT'\n" +
+            value = "select count(c.id), c.full_name " +
+                    "from sale s join client c on s.client_id = c.id " +
+                    "where s.status = 'DEBT' " +
                     "group by c.full_name;",
             nativeQuery = true
     )
     List<?> countOfDebtClients();
 
     @Query(
-            value = "select c.full_name as fullName, sum(s.whole_price) as amount\n" +
-                    "from sale s join client c on c.id = s.client_id\n" +
-                    "where s.created_at >= ?1 and s.created_at <= ?2\n" +
+            value = "select c.full_name as fullName, sum(s.whole_price) as amount " +
+                    "from sale s join client c on c.id = s.client_id " +
+                    "where s.created_at >= ?1 and s.created_at <= ?2 " +
                     "group by c.created_by, c.id;",
             nativeQuery = true
     )
